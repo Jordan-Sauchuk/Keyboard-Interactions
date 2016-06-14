b86110d476c14eb5c1b16a62a4537f127c385b10
Jordan S <goglobalpioneers@gmail.com>
2016-06-13 20:22:09 -0400

    add files via upload
    JS and CSS changes / alert and styling

---
+++ Patatap clone Demo/democlone.js
@@ -0,0 +1,10 @@
+window.alert("Press on any letter on your keyboard to create sounds and possibly your own music that could be the greatest song ever composed by man");
+
+
+
+//var r = confirm("Press a button");
+//if (r == true) {
+ //   x = "You pressed OK!";
+//} else {
+//    x = "You pressed Cancel!";
+//}
